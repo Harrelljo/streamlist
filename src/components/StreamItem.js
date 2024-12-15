890163c388148a7db5e789c45853d919570cd31f
Harrelljo <jontae.harrell@gmail.com>
2024-12-15 11:32:33 -0800

    Update StreamItem.js

--- src/components/StreamItem.js
+++ src/components/StreamItem.js
@@ -1,13 +1,15 @@
 // src/components/StreamItem.js
+
 import React from 'react';
 
-const StreamItem = ({ stream }) => {
+function StreamItem({ stream }) {
   return (
     <div className="stream-item">
       <h2>{stream.title}</h2>
       <p>{stream.description}</p>
+      {/* You can display other details, like image, link, etc. */}
     </div>
   );
-};
+}
 
 export default StreamItem;
