b842673e7af8732320d46b86aeb049b26b051ac5
Harrelljo <jontae.harrell@gmail.com>
2024-12-16 16:04:34 -0800

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,7 @@
+import React from "react";
+
 function Movies() {
-  return <h1>Movies Page</h1>;
+  return <div className="container"><h2>Movies Page - Coming Soon</h2></div>;
 }
 
 export default Movies;
