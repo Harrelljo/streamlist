ac96eab8283d7a14cf93aba1f0d0352c33cc584f
Harrelljo <jontae.harrell@gmail.com>
2024-12-16 16:03:44 -0800

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+function Navigation() {
+  return (
+    <nav className="navbar">
+      <h1>StreamList</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/movies">Movies</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
 
