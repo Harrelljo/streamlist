f48c5cd23e045bc51439eee6457a0969bdc39bee
Harrelljo <jontae.harrell@gmail.com>
2024-12-16 16:05:40 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-// src/App.js
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import StreamList from './StreamList';  // Make sure this file exists
-import Movies from './Movies';          // Make sure this file exists
-import Cart from './Cart';              // Make sure this file exists
-import About from './About';            // Make sure this file exists
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import StreamList from "./components/StreamList";
+import Movies from "./components/Movies";
+import Cart from "./components/Cart";
+import About from "./components/About";
+import "./styles.css";
+import "./fonts.css";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/movies">Movies</Link>
-            </li>
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
+        <Navigation />
         <Routes>
           <Route path="/" element={<StreamList />} />
           <Route path="/movies" element={<Movies />} />
